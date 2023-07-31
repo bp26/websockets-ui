2417f975214c823556377695f2f920094cb9cc4a
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-31 20:42:29 +0300

    feat: add index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { httpServer } from "./src/http_server/index.js";
+import server from './src/http_server';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+server.listen(HTTP_PORT);
