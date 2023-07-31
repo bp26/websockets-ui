9c43d6b30b5be56478407a28a1a2ec925f9b2636
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-31 20:41:14 +0300

    feat: add Service

---
+++ src/http_server/services/Service.ts
@@ -0,0 +1,23 @@
+import db from '../db/db';
+import { Player } from '../types/interfaces';
+
+class Service {
+  public register({ name, password }: Player) {
+    try {
+      const user = db.addPlayer({ name, password });
+      return {
+        ...user,
+        error: false,
+        errorText: '',
+      };
+    } catch (error) {
+      return {
+        name,
+        error: true,
+        errorText: (error as Error).message,
+      };
+    }
+  }
+}
+
+export default new Service();
