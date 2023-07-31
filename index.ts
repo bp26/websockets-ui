fe127b6f54cd199c266c9647fd8156a48e981801
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-31 22:30:03 +0300

    feat: set port to 3000

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import server from './src/http_server';
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 server.listen(HTTP_PORT);
