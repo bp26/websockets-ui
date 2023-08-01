f0729db68ec5e17b7876d03bb0d969602b712c89
bp26 <aleshak260006@gmail.com>
2023-08-01 07:44:33 +0000

    feat: separate method register in Service intp separate methods

--- src/http_server/services/Service.ts
+++ src/http_server/services/Service.ts
@@ -1,8 +1,20 @@
 import db from '../db/db';
 import { Player } from '../types/interfaces';
+import { ServerMessageMode } from '../types/enums';
 
 class Service {
-  public register({ name, password }: Player) {
+  public register(data: Player) {
+    return [
+      { mode: ServerMessageMode.SEND, data: this.formRegisterData(data) },
+      { mode: ServerMessageMode.BROADCAST, data: this.formRoomStateData() },
+    ];
+  }
+
+  private formRegisterData({ name, password }: Player) {
+    if (!name || !password) {
+      return this.formError('Wrong data in incoming message');
+    }
+
     try {
       const user = db.addPlayer({ name, password });
       return {
@@ -11,11 +23,19 @@ class Service {
         errorText: '',
       };
     } catch (error) {
-      return {
-        error: true,
-        errorText: (error as Error).message,
-      };
+      return this.formError(error.message);
     }
+  }
+
+  private formRoomStateData() {
+    return db.getRooms();
+  }
+
+  private formError(errorText: string) {
+    return {
+      error: true,
+      errorText,
+    };
   }
 }
 
