f80e316354e63f33ee5274a0cef70216a3f66275
bp26 <aleshak260006@gmail.com>
2023-08-01 18:00:15 +0000

    feat: add uuid id to websockets

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -1,24 +1,24 @@
 import { WebSocketServer } from 'ws';
 import { createServer } from 'http';
 import Controller from './controllers/Controller';
+import { v4 } from 'uuid';
+import { ExtWebSocket } from './types/interfaces';
 
 const server = createServer();
 
 const wsServer = new WebSocketServer({ server });
 
-wsServer.on('connection', (ws) => {
-  const controller = new Controller(wsServer);
+wsServer.on('connection', (ws: ExtWebSocket) => {
+  const id = v4();
+  ws.id = id;
+  const controller = new Controller(wsServer, ws);
 
   ws.on('message', (msg) => {
-    controller.handleMessage(JSON.parse(msg.toString()), ws);
+    controller.handleMessage(JSON.parse(msg.toString()));
   });
 
   ws.on('close', () => {
     controller.handleClose();
-  });
-
-  ws.on('error', () => {
-    controller.handleError();
   });
 });
 
