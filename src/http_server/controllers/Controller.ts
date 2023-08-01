8932c6e3aa1513dc6311d15c45009ad4aaa07648
bp26 <aleshak260006@gmail.com>
2023-08-01 18:02:10 +0000

    feat: update Controller

--- src/http_server/controllers/Controller.ts
+++ src/http_server/controllers/Controller.ts
@@ -1,24 +1,28 @@
 import { WebSocketServer, WebSocket } from 'ws';
 import { MessageType, ServerMessageMode } from '../types/enums';
-import { ArrangedData, Message } from '../types/interfaces';
+import { ArrangedData, Message, ExtWebSocket } from '../types/interfaces';
 import Service from '../services/Service';
 
 class Controller {
   private server: WebSocketServer;
+  private ws: ExtWebSocket;
 
-  constructor(server: WebSocketServer) {
+  constructor(server: WebSocketServer, ws: ExtWebSocket) {
     this.server = server;
+    this.ws = ws;
   }
 
-  public handleMessage({ type, data }: Message<string>, ws: WebSocket) {
+  public handleMessage({ type, data }: Message<string>) {
     switch (type) {
       case MessageType.REGISTER: {
-        const outgoingData = Service.register(JSON.parse(data));
-        this.dispatchMessages(ws, type, outgoingData);
+        const outgoingData = Service.register(JSON.parse(data), this.ws.id);
+        this.dispatchMessages(outgoingData);
         break;
       }
 
-      case MessageType.UPDATE_ROOM: {
+      case MessageType.CREATE_ROOM: {
+        const outgoingData = Service.createRoom();
+        this.dispatchMessages(outgoingData);
         break;
       }
 
@@ -29,19 +33,17 @@ class Controller {
 
   public handleClose() {}
 
-  public handleError() {}
-
-  private dispatchMessages(ws: WebSocket, type: string, outgoingData: ArrangedData[]) {
-    outgoingData.forEach((arrangedData) => {
-      switch (arrangedData.mode) {
+  private dispatchMessages(outgoingData: ArrangedData[]) {
+    outgoingData.forEach(({ mode, type, data }) => {
+      switch (mode) {
         case ServerMessageMode.SEND: {
-          ws.send(this.formatOutgoingMessage(type, arrangedData.data));
+          this.ws.send(this.generateOutgoingMessage(type, data));
           break;
         }
 
-        case ServerMessageMode.SEND: {
+        case ServerMessageMode.BROADCAST: {
           this.server.clients.forEach((client) => {
-            client.send(this.formatOutgoingMessage(type, arrangedData.data));
+            client.send(this.generateOutgoingMessage(type, data));
           });
           break;
         }
@@ -49,7 +51,7 @@ class Controller {
     });
   }
 
-  private formatOutgoingMessage(type: string, outgoingData: unknown) {
+  private generateOutgoingMessage(type: MessageType, outgoingData: unknown) {
     return JSON.stringify({
       type,
       data: JSON.stringify(outgoingData),
