098dd80794159b751dacccbad7759e7623443365
bp26 <aleshak260006@gmail.com>
2023-08-02 08:54:17 +0000

    feat: update method dispatchMessages in Controller

--- src/http_server/controllers/Controller.ts
+++ src/http_server/controllers/Controller.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer } from 'ws';
 import { MessageType, ServerMessageMode } from '../types/enums';
-import { ArrangedData, Message, ExtWebSocket } from '../types/interfaces';
+import { ArrangedMessage, Message, ExtWebSocket, DataArray } from '../types/interfaces';
 import Service from '../services/Service';
 
 class Controller {
@@ -13,31 +13,35 @@ class Controller {
   }
 
   public handleMessage({ type, data }: Message<string>) {
-    switch (type) {
-      case MessageType.REGISTER: {
-        const outgoingData = Service.handleRegister(JSON.parse(data), this.ws.id);
-        this.dispatchMessages(outgoingData);
-        break;
-      }
+    try {
+      switch (type) {
+        case MessageType.REGISTER: {
+          const outgoingData = Service.handleRegister(JSON.parse(data), this.ws.id);
+          this.dispatchMessages(outgoingData);
+          break;
+        }
 
-      case MessageType.CREATE_ROOM: {
-        const outgoingData = Service.handleCreateRoom(this.ws.id);
-        this.dispatchMessages(outgoingData);
-        break;
-      }
+        case MessageType.CREATE_ROOM: {
+          const outgoingData = Service.handleCreateRoom(this.ws.id);
+          this.dispatchMessages(outgoingData);
+          break;
+        }
 
-      case MessageType.ADD_USER: {
-        const outgoingData = Service.handleAddUser(JSON.parse(data), this.ws.id);
-        this.dispatchMessages(outgoingData);
-        break;
+        case MessageType.ADD_USER: {
+          const outgoingData = Service.handleAddUser(JSON.parse(data), this.ws.id);
+          this.dispatchMessages(outgoingData);
+          break;
+        }
       }
+    } catch (error) {
+      console.log(error);
     }
   }
 
   public handleClose() {}
 
-  private dispatchMessages(outgoingData: ArrangedData[]) {
-    outgoingData.forEach(({ mode, type, data }) => {
+  private dispatchMessages(outgoingData: ArrangedMessage[]) {
+    outgoingData.forEach(({ mode, type, data, wsIds }) => {
       switch (mode) {
         case ServerMessageMode.SEND: {
           this.ws.send(this.generateOutgoingMessage(type, data));
@@ -48,6 +52,34 @@ class Controller {
           this.server.clients.forEach((client) => {
             client.send(this.generateOutgoingMessage(type, data));
           });
+          break;
+        }
+
+        case ServerMessageMode.BROADCAST_SELECTIVE: {
+          if (wsIds) {
+            this.server.clients.forEach((client) => {
+              if (wsIds.includes((client as ExtWebSocket).id)) {
+                client.send(this.generateOutgoingMessage(type, data));
+              }
+            });
+          }
+          break;
+        }
+
+        case ServerMessageMode.BROADCAST_SELECTIVE_CYCLE_DATA: {
+          if (wsIds) {
+            this.server.clients.forEach((client) => {
+              const clientId = (client as ExtWebSocket).id;
+              if (wsIds.includes(clientId)) {
+                client.send(
+                  this.generateOutgoingMessage(
+                    type,
+                    (data as DataArray).find((item) => item.id === clientId)
+                  )
+                );
+              }
+            });
+          }
           break;
         }
       }
