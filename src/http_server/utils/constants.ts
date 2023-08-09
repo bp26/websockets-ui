ddf3c502526e73fe1c97eddc7ce3b1d809aa9158
bp26 <94074742+bp26@users.noreply.github.com>
2023-08-09 12:55:35 +0300

    feat: add constant ERROR_GAME_PLAYER_NOT_IN_GAME

--- src/http_server/utils/constants.ts
+++ src/http_server/utils/constants.ts
@@ -1,5 +1,6 @@
-export const ERROR_PLAYER_NOT_LOGGED_IN = `Player isnt't logged in`;
+export const ERROR_PLAYER_NOT_LOGGED_IN = `Player isn't logged in`;
 export const ERROR_ROOM_DOESNT_EXIST = `This room doesn't exist`;
 export const ERROR_PLAYER_WRONG_PASSWORD = 'Wrong password for this player';
 export const ERROR_PLAYER_ALREADY_ONLINE = 'This player is already online';
 export const ERROR_GAME_NOT_CREATED = `A game wasn't created for this player`;
+export const ERROR_GAME_PLAYER_NOT_IN_GAME = `This player isn't in this game`;
