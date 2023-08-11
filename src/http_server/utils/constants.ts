f8a1cb2b8c035d9a4491c416ff629a1ea95b82d8
bp26 <94074742+bp26@users.noreply.github.com>
2023-08-11 14:52:23 +0300

    feat: add error messages constants

--- src/http_server/utils/constants.ts
+++ src/http_server/utils/constants.ts
@@ -1,6 +1,8 @@
 export const ERROR_PLAYER_NOT_LOGGED_IN = `Player isn't logged in`;
-export const ERROR_ROOM_DOESNT_EXIST = `This room doesn't exist`;
 export const ERROR_PLAYER_WRONG_PASSWORD = 'Wrong password for this player';
 export const ERROR_PLAYER_ALREADY_ONLINE = 'This player is already online';
+export const ERROR_ROOM_DOESNT_EXIST = `This room doesn't exist`;
 export const ERROR_GAME_NOT_CREATED = `A game wasn't created for this player`;
 export const ERROR_GAME_PLAYER_NOT_IN_GAME = `This player isn't in this game`;
+export const ERROR_GAME_NOT_STARTED = `This game hasn't yet been started`;
+export const ERROR_GAME_WRONG_PLAYER = `It isn't this player's turn to attack`;
