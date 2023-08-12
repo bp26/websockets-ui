2496bcf7c06a7ea95339049495b0152e28ba1c9b
bp26 <94074742+bp26@users.noreply.github.com>
2023-08-12 22:56:10 +0300

    feat: add constants

--- src/http_server/utils/constants.ts
+++ src/http_server/utils/constants.ts
@@ -1,8 +1,18 @@
 export const ERROR_PLAYER_NOT_LOGGED_IN = `Player isn't logged in`;
 export const ERROR_PLAYER_WRONG_PASSWORD = 'Wrong password for this player';
 export const ERROR_PLAYER_ALREADY_ONLINE = 'This player is already online';
+
 export const ERROR_ROOM_DOESNT_EXIST = `This room doesn't exist`;
+export const ERROR_ROOM_PLAYER_ALREADY_IN_ROOM = `Player has already been added to this room`;
+
 export const ERROR_GAME_NOT_CREATED = `A game wasn't created for this player`;
 export const ERROR_GAME_PLAYER_NOT_IN_GAME = `This player isn't in this game`;
 export const ERROR_GAME_NOT_STARTED = `This game hasn't yet been started`;
 export const ERROR_GAME_WRONG_PLAYER = `It isn't this player's turn to attack`;
+export const ERROR_GAME_SHIPS_ALREADY_ADDED = `Ships were already added for this player`;
+
+export const UNEXPECTED_ERROR_GAME_DOESNT_EXIST = `Unexpected error: Game doesn't exist`;
+export const UNEXPECTED_ERROR_ENEMY_DOESNT_EXIST = `Unexpected error: Enemy player doesn't exist`;
+
+export const FIELD_MIN_COUNT = 0;
+export const FIELD_MAX_COUNT = 9;
