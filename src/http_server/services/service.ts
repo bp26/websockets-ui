2645448f79189b61396997e5ec76c8eedc7bd8e0
bp26 <94074742+bp26@users.noreply.github.com>
2023-08-12 23:01:14 +0300

    feat: add handleRandomAttack and handleWin methods in Service

--- src/http_server/services/service.ts
+++ src/http_server/services/service.ts
@@ -2,17 +2,22 @@ import { playerService } from './player.service';
 import { roomService } from './room.service';
 import { gameService } from './game.service';
 import { winnerService } from './winner.service';
-import { ArrangedMessage, AttackData, GameData, PlayerData, RoomData } from '../types/interfaces';
-import { MessageType, ServerMessageMode } from '../types/enums';
+import { ArrangedMessage, AttackData, GameData, PlayerData, RandomAttackData, RoomData } from '../types/interfaces';
+import { AttackResult, MessageType, ServerMessageMode } from '../types/enums';
 import { ServerError } from '../utils/ServerError';
 import { ERROR_PLAYER_NOT_LOGGED_IN } from '../utils/constants';
+import { Player } from '../db/player.db';
 
 class Service {
   public handleRegister(data: PlayerData, playerId: string): ArrangedMessage[] {
     const registerData = playerService.register(data, playerId);
 
     return [
-      { mode: ServerMessageMode.SEND, data: registerData, type: MessageType.REGISTER },
+      {
+        mode: ServerMessageMode.SEND,
+        data: registerData,
+        type: MessageType.REGISTER,
+      },
       { mode: ServerMessageMode.BROADCAST, data: roomService.getRooms(), type: MessageType.UPDATE_ROOM },
       { mode: ServerMessageMode.BROADCAST, data: winnerService.getWinners(), type: MessageType.UPDATE_WINNERS },
     ];
@@ -26,7 +31,14 @@ class Service {
     }
 
     roomService.createRoom(player);
-    return [{ mode: ServerMessageMode.BROADCAST, data: roomService.getRooms(), type: MessageType.UPDATE_ROOM }];
+
+    return [
+      {
+        mode: ServerMessageMode.BROADCAST,
+        data: roomService.getRooms(),
+        type: MessageType.UPDATE_ROOM,
+      },
+    ];
   }
 
   public handleAddUser({ indexRoom: roomId }: RoomData, playerId: string): ArrangedMessage[] {
@@ -47,7 +59,7 @@ class Service {
     ];
 
     if (isRoomFull) {
-      const { data, id: gameId } = gameService.createGame(playersIds[0], playersIds[1]);
+      const { data, gameId } = gameService.createGame(playersIds[0], playersIds[1]);
       playersIds.forEach((id) => playerService.addGameIdToPlayer(id, gameId));
       arrangedMessages.push({
         mode: ServerMessageMode.BROADCAST_SELECTIVE_CYCLE_DATA,
@@ -98,26 +110,66 @@ class Service {
       throw new ServerError(ERROR_PLAYER_NOT_LOGGED_IN);
     }
 
-    const { nextPlayer, data: outgoingData, playersIds } = gameService.performAttack(data);
+    const { status, nextPlayer, playersIds, shotData, killedShipData, areShipsEmpty } = gameService.performAttack(data);
 
-    return [
-      {
+    const arrangedMessages: ArrangedMessage[] = [];
+
+    if (status === AttackResult.KILLED && killedShipData) {
+      arrangedMessages.push(
+        ...killedShipData.map((data) => {
+          return {
+            mode: ServerMessageMode.BROADCAST_SELECTIVE,
+            data,
+            type: MessageType.ATTACK,
+            wsIds: playersIds,
+          };
+        })
+      );
+    } else {
+      arrangedMessages.push({
         mode: ServerMessageMode.BROADCAST_SELECTIVE,
-        data: outgoingData,
+        data: shotData,
         type: MessageType.ATTACK,
         wsIds: playersIds,
-      },
-      {
+      });
+    }
+
+    if (areShipsEmpty) {
+      return this.handleWin(data, player, playersIds);
+    } else {
+      arrangedMessages.push({
         mode: ServerMessageMode.BROADCAST_SELECTIVE,
         data: gameService.beginTurn(data.gameId, nextPlayer),
         type: MessageType.TURN,
         wsIds: playersIds,
-      },
-    ];
+      });
+    }
+
+    return arrangedMessages;
+  }
+
+  public handleRandomAttack(data: RandomAttackData) {
+    const { x, y } = gameService.generateRandomCoordinates();
+    return this.handleAttack({ ...data, x, y });
   }
 
   public handleClose(playerId: string) {
     playerService.logoffPlayer(playerId);
+  }
+
+  private handleWin(gameData: AttackData, player: Player, playersIds: string[]) {
+    playersIds.forEach((id) => playerService.addGameIdToPlayer(id, ''));
+    winnerService.addWin(player.name);
+
+    return [
+      {
+        mode: ServerMessageMode.BROADCAST_SELECTIVE,
+        data: gameService.finishGame(gameData.gameId, gameData.indexPlayer),
+        type: MessageType.FINISH,
+        wsIds: playersIds,
+      },
+      { mode: ServerMessageMode.BROADCAST, data: winnerService.getWinners(), type: MessageType.UPDATE_WINNERS },
+    ];
   }
 }
 
